2072ffd8dcc899ee81acaa25d2af3b5444ac6884
mstoro <mstoro@softserveinc.com>
2021-11-12 11:03:03 +0200

    [CCTRI-3121] fix vulnerabilities

--- code/api/rest_api_client.py
+++ code/api/rest_api_client.py
@@ -38,7 +38,7 @@ class RestApiClient:
     def _get(self, endpoint, headers=None, params=None):
         response = requests.get(
             f'https://{self.server_ip}{self.base_uri}{endpoint}',
-            headers=headers, verify=False, auth=self.credentials,
+            headers=headers, auth=self.credentials,
             params=params)
         return self._get_response_data(response)
 
@@ -46,12 +46,12 @@ class RestApiClient:
     def _post(self, endpoint, data=None):
         response = requests.post(
             f'https://{self.server_ip}{self.base_uri}{endpoint}',
-            data, headers=self.headers, verify=False, auth=self.credentials)
+            data, headers=self.headers, auth=self.credentials)
         return self._get_response_data(response)
 
     @handle_connection_error
     def _delete(self, endpoint, headers):
         response = requests.delete(
             f'https://{self.server_ip}{self.base_uri}{endpoint}',
-            headers=headers, verify=False, auth=self.credentials)
+            headers=headers, auth=self.credentials)
         return self._get_response_data(response)
